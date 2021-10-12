24b9969288072ea9a6b38af0b73dbf26372aed57
the-real-adammork <adammork@gmail.com>
2021-10-12 11:23:51 -0700

    specify versions for llvm swift and rust

--- config.sh
+++ config.sh
@@ -3,14 +3,14 @@
 # returned by the command below:
 # $ xcrun -sdk iphoneos swiftc --version
 
-LLVM_BRANCH="tags/swift-5.3.2-RELEASE"
+LLVM_BRANCH="tags/swift-5.4.2-RELEASE"
 
 # 2. Select the best branch, tag or commit hash from https://github.com/rust-lang/rust
 
-RUST_BRANCH="a8486b64b" # nightly-2021-02-25
+RUST_BRANCH="f63ec777bcfdedb752f55ac676ac0724c6cef47e" # nightly-2021-07-25
 
 # 3. Select a name for the toolchain you want to install as. The toolchain will be installed
 # under $HOME/.rustup/toolchains/rust-$RUST_TOOLCHAIN
 
-RUST_TOOLCHAIN="ios-arm64-nightly-2021-02-25"
+RUST_TOOLCHAIN="ios-arm64-nightly-2021-07-25-swift-5-4-2"
 
